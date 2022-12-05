ac9ebf3ab0a30413b147d0a504b78d6eb002af53
Alex <as1857@ship.edu>
2022-12-05 13:01:49 -0500

    Finished player tests

--- src/test/java/edu/ship/engr/display/entity/PlayerTest.java
+++ src/test/java/edu/ship/engr/display/entity/PlayerTest.java
@@ -2,9 +2,11 @@ package edu.ship.engr.display.entity;
 
 import edu.ship.engr.display.Board;
 import edu.ship.engr.enums.Direction;
+import edu.ship.engr.exceptions.InvalidTargetException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Test for the player class
@@ -27,7 +29,46 @@ public class PlayerTest {
         assertEquals(Direction.Right, player.getDirection());
     }
 
-    public void testPlaceBox() {
+    @Test
+    public void testTargetPosition() {
         Board board = Board.getInstance();
+        Player p = board.getMyPlayer();
+        p.setPosition(3, 3);
+
+        p.setDirection(Direction.Down);
+        int[] targetPos = p.getTargetPosition();
+        assertEquals(3, targetPos[0]);
+        assertEquals(4, targetPos[1]);
+
+        p.setDirection(Direction.Up);
+        targetPos = p.getTargetPosition();
+        assertEquals(3, targetPos[0]);
+        assertEquals(2, targetPos[1]);
+
+        p.setDirection(Direction.Right);
+        targetPos = p.getTargetPosition();
+        assertEquals(4, targetPos[0]);
+        assertEquals(3, targetPos[1]);
+
+        p.setDirection(Direction.Left);
+        targetPos = p.getTargetPosition();
+        assertEquals(2, targetPos[0]);
+        assertEquals(3, targetPos[1]);
+    }
+
+    @Test
+    public void testPlaceBox() throws InvalidTargetException {
+        Board board = Board.getInstance();
+        Player p = board.getMyPlayer();
+        p.setPosition(3, 3);
+        p.setDirection(Direction.Down);
+        // doesn't matter about coords as long as they are different than where we are placing
+        Box b = new Box(2, 2);
+        p.pickupBox(b);
+
+        p.placeBox();
+        assertFalse(b.getState());
+        assertEquals(3, b.getXPos());
+        assertEquals(4, b.getYPos());
     }
 }
